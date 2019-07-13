ba1ad8d3126107ea9fb2c3a937ceb6f243d15753
Aleksey Senilov <thenn.erannor@gmail.com>
2019-07-13 17:55:45 +0300

    Update packages

--- backend/DB2019.Backend/DB2019.Backend.Api/Controllers/HomeController.cs
+++ backend/DB2019.Backend/DB2019.Backend.Api/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace DB2019.Backend.Api.Controllers
 {
@@ -10,9 +6,7 @@ namespace DB2019.Backend.Api.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Title = "Home Page";
-
-            return View();
+            return RedirectToAction("Index", "Help");
         }
     }
 }
