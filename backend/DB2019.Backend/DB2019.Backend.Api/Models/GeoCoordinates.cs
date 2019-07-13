e32e77115dc18f7d2607bac52f8e480ba224ebeb
Aleksey Senilov <thenn.erannor@gmail.com>
2019-07-13 19:11:57 +0300

    Coordinates

---
+++ backend/DB2019.Backend/DB2019.Backend.Api/Models/GeoCoordinates.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DB2019.Backend.Api.Models
+{
+    /// <summary>
+    ///     Гео-координаты
+    /// </summary>
+    public class GeoCoordinates
+    {
+        /// <summary>
+        ///     Широта
+        /// </summary>
+        [Required]
+        public double Latitude { get; set; }
+
+        /// <summary>
+        ///     Долгота
+        /// </summary>
+        [Required]
+        public double Longitude { get; set; }
+    }
+}
--- backend/DB2019.Backend/DB2019.Backend.Api/Models/NewIssueData.cs
+++ backend/DB2019.Backend/DB2019.Backend.Api/Models/NewIssueData.cs
@@ -3,23 +3,31 @@
 namespace DB2019.Backend.Api.Models
 {
     /// <summary>
-    /// Параметры новой заявки
+    ///     Параметры новой заявки
     /// </summary>
     public class NewIssueData
     {
         /// <summary>
-        /// Категория заявки
+        ///     Категория заявки
         /// </summary>
         [Required]
         public string Category { get; set; }
+
         /// <summary>
-        /// Фотография к заявке, в base64 формате
+        ///     Фотография к заявке, в base64 формате
         /// </summary>
         [Required]
         public string Photo { get; set; }
+
         /// <summary>
-        /// Необязательный комментарий к заявке
+        ///     Необязательный комментарий к заявке
         /// </summary>
         public string Comment { get; set; }
+
+        /// <summary>
+        ///     Гео-позиция пользователя
+        /// </summary>
+        [Required]
+        public GeoCoordinates Position { get; set; }
     }
 }
