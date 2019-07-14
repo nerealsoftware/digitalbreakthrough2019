ca9612cd65b28b94aac4df6013329f2c87525cd7
Алексей Сенилов <thenn.erannor@gmail.com>
2019-07-14 11:47:04 +0300

    Fixes

--- backend/DB2019.Backend/DB2019.Backend.Api/Controllers/IssueController.cs
+++ backend/DB2019.Backend/DB2019.Backend.Api/Controllers/IssueController.cs
@@ -45,7 +45,6 @@ namespace DB2019.Backend.Api.Controllers
                 if (user == null) return Unauthorized();
                 var category = db.Categories.FirstOrDefault(c => c.Id == data.CategoryId);
                 if (category == null) return BadRequest("Invalid category id");
-                if (data.Position == null) return BadRequest("Invalid position");
                 byte[] photo = null;
                 try
                 {
@@ -64,8 +63,8 @@ namespace DB2019.Backend.Api.Controllers
                 {
                     User = user,
                     Category = category,
-                    Latitude = data.Position.Latitude,
-                    Longitude = data.Position.Longitude,
+                    Latitude = data.Latitude,
+                    Longitude = data.Longitude,
                     Photo = photo,
                     Comment = string.IsNullOrWhiteSpace(data.Comment) ? null : data.Comment.Trim()
                 };
@@ -77,16 +76,20 @@ namespace DB2019.Backend.Api.Controllers
                 db.Issues.Add(issue);
                 db.SaveChanges();
 
-                return Ok();
+                return Ok(
+                    new
+                    {
+                        Message = "Ok"
+                    });
             }
         }
 
         /// <summary>
         /// Получить список заявок
         /// </summary>
-        /// <param name="categoryId">Идентификатор категории</param>
         /// <param name="framePosition">Номер первого элемента</param>
         /// <param name="frameSize">Количество запрашиваемых элементов</param>
+        /// <param name="categoryId">Идентификатор категории</param>
         /// <returns></returns>
         public Frame<IssueData> Get(int framePosition, int frameSize, int? categoryId = null)
         {
@@ -125,16 +128,30 @@ namespace DB2019.Backend.Api.Controllers
 
                 issue.Rating += 1;
                 db.SaveChanges();
-                return Ok();
+                return Ok(
+                    new
+                    {
+                        Message = "Ok"
+                    });
             }
         }
 
+        /// <summary>
+        ///     Получить заявки по координатам
+        /// </summary>
+        /// <param name="framePosition">Номер первого элемента</param>
+        /// <param name="frameSize">Количество запрашиваемых элементов</param>
+        /// <param name="latitude">Широта</param>
+        /// <param name="longitude">Долгота</param>
+        /// <param name="radius">Радиус (тоже в десятичных градусах, поэтому нужно маленькое значение)</param>
+        /// <returns></returns>
+        [Route("api/issue/position")]
         public Frame<IssueData> GetByPosition(
             int framePosition,
             int frameSize,
             double latitude,
             double longitude,
-            double radios )
+            double radius )
         {
             return null;
         }
@@ -154,7 +171,7 @@ namespace DB2019.Backend.Api.Controllers
             {
                 var query = db.Issues.Include(i => i.Tags);
                 if (categoryId.HasValue) query = query.Where(i => i.CategoryId == categoryId.Value);
-
+                query = query.OrderBy( i => i.Id );
                 if (frameSize >= 0) query = query.Skip(framePosition).Take(frameSize);
 
                 var issues = query.ToList();
